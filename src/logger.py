faa0aa1b83d66f5ae9f58857dd8b45d5f7b6fa23
subupkrl <139966058+subupkrl@users.noreply.github.com>
2026-01-20 13:35:43 +0545

    setup and logger code written

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging 
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    format='[%(asctime)s] %(lineno)d - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+if __name__ == "__main__":
+    logging.info('Logging has started')
